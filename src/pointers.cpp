32489343a4e3b4c326da491df156f2e4e7747835
Ph0enixz <Ph0enixz@users.noreply.github.com>
2019-03-25 18:53:19 -0400

    Fixed it

--- src/pointers.cpp
+++ src/pointers.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 void myswap(int * ptr1, int* ptr2)
 {
-  auto temp = ptr1;
-  ptr1 = ptr2;
-  ptr2 = temp;
+  auto temp = *ptr1;
+  *ptr1 = *ptr2;
+  *ptr2 = temp;
 }
 
 
@@ -14,13 +14,13 @@ int main()
 {
   int a = 25, b = 11;    
 
-  cout<<"What am I doing wrong ☹ \n\n";
-
+  //cout<<"What am I doing wrong ☹ \n\n";
+	cout << "Fixed it! :) \n\n"; 
   cout<<"a = "<<a<<", b = "<<b<<endl;
 
 //swap(a,b); //why does theirs work????   
  
-  myswap(a, b); // but mine doesn't ?!?!?!?!!!???
+  myswap(&a, &b); // but mine doesn't ?!?!?!?!!!???
 
   cout<<"a = "<<a<<", b = "<<b<<endl;
 
